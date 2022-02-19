4392298fc357756ba5620b9d8a38b35a5b83efef
Luis Felipe <202makertv@gmail.com>
2022-02-19 16:07:34 -0300

    Correção do arquivo get_posts.php

--- api/get_posts.php
+++ api/get_posts.php
@@ -1,13 +1,13 @@
 <?php 
 header('Access-Control-Allow-Origin: *');
-if($_GET['posts']){
+if(isset($_GET['posts'])){
     if($_GET['categoria']){
         die("W.I.P");
     }else{
         $sql = MySql::connect()->prepare("SELECT * FROM `tb_posts`");
         $sql->execute();
         $data = $sql->fetchAll();
-        die(json_encode($data));
+        echo json_encode($data);
     }
 }else{
     die("Requisição rejeitada");
