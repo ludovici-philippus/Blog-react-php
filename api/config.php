df4028ded2aac9f9abcb7fd8b0a79f0f1478793f
Luis Felipe <37634205+ludovici-philippus@users.noreply.github.com>
2022-02-19 18:44:08 +0000

    Update config.php

--- api/config.php
+++ api/config.php
@@ -1,5 +1,4 @@
 <?php
-    session_start();
     ini_set('display_errors', 1);
     error_reporting(E_ALL);
     date_default_timezone_set("America/Sao_Paulo");
@@ -10,10 +9,6 @@
 
     spl_autoload_register($autoload);
 
-    define("INCLUDE_PATH", "http://localhost/loja-virtual-teste/");
-    define("INCLUDE_PATH_PUBLIC", INCLUDE_PATH."private_space/views/pages/");
-    define("INCLUDE_PATH_PAINEL", INCLUDE_PATH."admin/");
-    define("INCLUDE_PATH_PAINEL_PUBLIC", INCLUDE_PATH."admin/private_space/views/pages/");
     define("DATA", array("host"=>"localhost","dbname"=>"blog_react","user"=>"root","pass"=>""));
 
     define("IN_PRODUCTION", false);
